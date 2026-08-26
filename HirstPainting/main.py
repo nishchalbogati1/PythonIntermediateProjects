7525031928fd6ed9484d9a521c8e41a97a48c176
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-08-26 11:54:07 +0545

    Adding the main painting code

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -1,14 +1,36 @@
-import colorgram
+# import colorgram
+#
+# rgb_colors = []
+# colors =  colorgram.extract("image.jpg", 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-rgb_colors = []
-colors =  colorgram.extract("image.jpg", 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-
-print(rgb_colors)
+import turtle as t
+import random
 
 color = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+tur = t.Turtle()
+tur.shape("turtle")
+x = -200
+y = -200
+t.colormode(255)
+tur.speed(5)
+tur.penup()
+tur.goto(x,y)
+for _ in range(10):
+    for _ in range(10):
+        tur.dot(20,random.choice(color))
+        tur.forward(50)
+    y += 50
+    tur.goto(x,y)
+
+tur.hideturtle()
+
+screen = t.Screen()
+screen.exitonclick()
