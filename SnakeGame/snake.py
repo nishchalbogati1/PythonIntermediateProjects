cbea89eaa9a2e7fe5c860af504f272069e12e75b
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-09-15 16:26:50 +0545

    Added methods to control the movement of the snake using the Arrow Keys

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,11 +1,20 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
-        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.positions = []
+        self.create_snake()
+        self.head = self.positions[0]
 
-        for position in starting_positions:
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
             new_position = Turtle("square")
             new_position.penup()
             new_position.color("white")
@@ -17,5 +26,20 @@ class Snake:
             new_x = self.positions[pos_num - 1].xcor()
             new_y = self.positions[pos_num - 1].ycor()
             self.positions[pos_num].goto(new_x, new_y)
-        self.positions[0].forward(20)
+        self.positions[0].forward(MOVE_DISTANCE)
 
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
