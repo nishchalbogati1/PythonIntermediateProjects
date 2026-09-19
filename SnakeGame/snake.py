ab5b5b6a4198b53f7b2bca365291b91c61b1259c
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-09-19 16:43:20 +0545

    Fixed snake reversing into itself on rapid key presses

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -9,37 +9,42 @@ RIGHT = 0
 
 class Snake:
     def __init__(self):
-        self.positions = []
+        self.segments = []
         self.create_snake()
-        self.head = self.positions[0]
+        self.head = self.segments[0]
+        self.direction = RIGHT        # direction of the last actual move
+        self.next_direction = RIGHT   # direction requested for the next move
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
             new_position = Turtle("square")
+            new_position.shapesize(stretch_wid=0.5, stretch_len=0.5)
             new_position.penup()
             new_position.color("white")
             new_position.goto(position)
-            self.positions.append(new_position)
+            self.segments.append(new_position)
 
     def move(self):
-        for pos_num in range(len(self.positions) - 1, 0, -1):
-            new_x = self.positions[pos_num - 1].xcor()
-            new_y = self.positions[pos_num - 1].ycor()
-            self.positions[pos_num].goto(new_x, new_y)
-        self.positions[0].forward(MOVE_DISTANCE)
+        self.direction = self.next_direction
+        self.head.setheading(self.direction)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
+        if self.direction != DOWN:
+            self.next_direction = UP
 
     def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
+        if self.direction != UP:
+            self.next_direction = DOWN
 
     def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
+        if self.direction != RIGHT:
+            self.next_direction = LEFT
 
     def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
+        if self.direction != LEFT:
+            self.next_direction = RIGHT
