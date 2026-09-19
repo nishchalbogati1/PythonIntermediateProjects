8d0899a5df204c15b3830f01405e0fcbbf1a8b26
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-09-19 17:13:16 +0545

    Added the score update and game over logic

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -13,6 +13,10 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align="center", font=("Arial", 14, "normal"))
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game over!", align="center", font=("Arial", 20, "normal"))
+
     def increase_score(self):
         self.score += 1
         self.clear()
